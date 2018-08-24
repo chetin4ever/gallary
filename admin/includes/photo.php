a4e53dcf3cd7296acc74ae49a0c74e3f59bb882d
chetan mahajan <hetan009@gmail.com>
2018-08-24 17:52:50 +0530

    db name and table fields added photo.php

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,9 +1,9 @@
 <?php
 
 class Photo extends Db_object{
-    protected static $db_table="photo"
+    protected static $db_table="photo";
     protected static $db_table_field=array('photo_id','title','description','filename','type','size');
-    
+  
     public $photo_id;
     public $title;
     public $description;
