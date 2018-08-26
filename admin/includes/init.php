55617a9fb381cab7a2e897d4ffb70b903a8737cb
chetan mahajan <hetan009@gmail.com>
2018-08-26 15:06:45 +0530

    delete path file and setup with php debug environment

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,7 +1,7 @@
 <?php
 	defined('DS') ? null : define('DS',DIRECTORY_SEPARATOR);
 	defined('DS') ? null : define('SITE_ROOT',DS .'xampp' . DS .'htdocs'. DS .'gallary');
-	defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', DS .'xampp'. DS .'htdocs' . DS . 'gallary'. DS .'gallary');
+	defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', DS .'xampp'. DS .'htdocs' . DS . 'gallary'. DS .'includes');
 	echo INCLUDES_PATH;
 	include("functions.php");
 	include("new_config.php");
@@ -11,4 +11,3 @@
 	include("photo.php");
 	include("session.php");
 ?>
-C:\xampp\htdocs\gallary\admin\font-awesome
